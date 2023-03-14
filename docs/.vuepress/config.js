ce72d1c975d23b960550aa733d7879dea023e554
YangWu Liao <81912331+Licodeao@users.noreply.github.com>
2023-03-14 16:40:43 +0800

    feat: finish

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,21 @@
 module.exports = {
+  title: "Lico-UI",
   base: "/licoUI/",
   themeConfig: {
+    nav: [
+      {
+        text: "Home",
+        link: "/",
+      },
+      {
+        text: "Github",
+        link: "https://github.com/Licodeao/licoUI",
+      },
+      {
+        text: "Npm",
+        link: "https://www.npmjs.com/package/lico-ui",
+      },
+    ],
     sidebar: ["/", "/componentDocs/card"],
   },
 };
